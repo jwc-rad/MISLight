1e39d67fc78de9d2c36af7d2f2dfb85ad3cb2a8e
Jae Won Choi <djc0105@gmail.com>
2022-06-30 14:01:40 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name='mislight',
+      packages=find_namespace_packages(include=["mislight", "mislight.*"]),
+      version='0.1.0',
+      description='MISLight. Medical Image Segmentation in Pytorch Lightning.',
+      author='Jae Won Choi',
+      license='MIT License',
+      install_requires=[
+            "torch>1.10.0",
+            "pytorch-lightning",
+            "scikit-image",
+            "SimpleITK==2.0.2",
+            "parmap",
+            "connected-components-3d",
+            "fastremap",
+      ],
+      )
