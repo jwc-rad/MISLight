2db7738ba09211b197daaf72da2b325d95bfef07
Jae Won Choi <djc0105@gmail.com>
2022-06-30 14:20:48 +0900

    Create predict_onestage_s.sh

---
+++ docker/predict_onestage_s.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+GPU=0
+DATAROOT="./inputs"
+RUN_DIR="./outputs"
+BATCH_SIZE=1
+PATH_DS="./parameters"
+PATH_CK="./parameters"
+
+python -m mislight.scripts.test --no_saveoptions --gpu_ids $GPU --dataroot $DATAROOT --run_base_dir $RUN_DIR --batch_size $BATCH_SIZE --train_ds_info $PATH_DS --load_pretrained $PATH_CK --force_cpu_process --inference_model 2
+python -m mislight.scripts.postprocess --no_saveoptions --gpu_ids $GPU --dataroot $DATAROOT --run_base_dir $RUN_DIR --batch_size $BATCH_SIZE --train_ds_info $PATH_DS --load_pretrained $PATH_CK --force_cpu_process
