e0897757ea05476f90a3815738dbc5c0893bd65d
Jae Won Choi <djc0105@gmail.com>
2023-01-03 23:25:36 +0900

    Initial

---
+++ mislight/models/base.py
@@ -0,0 +1,58 @@
+import torch
+import pytorch_lightning as pl
+
+class BaseModel(pl.LightningModule):
+    def __init__(self, opt):
+        super().__init__()
+        self.save_hyperparameters()
+        
+        # define networks
+        # define loss functions
+        # define metric
+            
+    def load_pretrained(self, path):
+        self.load_pretrained_nets(path, nets=self.net_names)
+    
+    def load_pretrained_nets(self, path, nets=[]):
+        '''For loading state_dict of part of the model.
+        Loading full model should be done by "load_from_checkpoint" (Lightning)
+        '''
+        
+        device = next(self.parameters()).device
+        
+        # load from checkpoint or state_dict
+        print(f'trying to load pretrained from {path}')
+        try:
+            state_dict = torch.load(path, map_location=device)['state_dict']
+        except:
+            state_dict = torch.load(path, map_location=device)
+        
+        if len(nets)==0:
+            self.load_state_dict(state_dict)
+        
+        all_keys_match = True
+        changed = False
+        for name in nets:
+            if hasattr(self, name):
+                net = getattr(self, name)
+                new_weights = net.state_dict()
+                
+                # first check if pretrained has all keys
+                keys_match = True
+                for k in new_weights.keys():
+                    if not f'{name}.{k}' in state_dict.keys():
+                        keys_match = False
+                        all_keys_match = False
+                        print(f"not loading {name} because keys don't match")
+                        break
+                if keys_match:
+                    for k in new_weights.keys():
+                        new_weights[k] = state_dict[f'{name}.{k}']
+                    net.load_state_dict(new_weights)
+                    changed = True
+                        
+        if changed:
+            if all_keys_match:
+                print('<All keys matched successfully>')
+        else:
+            print(f'nothing is loaded from {path}')
